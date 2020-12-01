d7251c05e66c0d277b425ed91e79b9a8a7810fd2
Felipe <felipesepulveda30@gmail.com>
2020-12-01 13:14:56 -0300

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:mastergame120@cluster0.v3vgi.mongodb.net/cafe';
+    //urlDB = 'mongodb+srv://admin:mastergame120@cluster0.v3vgi.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
