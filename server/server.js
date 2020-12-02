584eca6348ec79274fb760d48288c831f7364459
Felipe <felipesepulveda30@gmail.com>
2020-12-02 14:21:39 -0300

    Se devuelve al puerto 5000

--- server/server.js
+++ server/server.js
@@ -28,6 +28,6 @@ mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: tru
 
 
 //app.listen(3000, () => {
-app.listen((process.env.PORT || 8080), () => {
+app.listen((process.env.PORT || 5000), () => {
     console.log('Escuchando puerto: ', process.env.PORT);
 });
